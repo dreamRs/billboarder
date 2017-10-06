6f974f3133311e7e97f614590a17bd6ab26736b7
Victor Perrier <victor.perrier@dreamrs.fr>
2017-10-06 18:08:45 +0200

    mapping (next)

--- R/billboard-mapping.R
+++ R/billboard-mapping.R
@@ -40,24 +40,74 @@
 #   bb
 # }
 bb_aes <- function(bb, x, y, group = NULL) {
-  aes <- structure(as.list(match.call()[-1]), class = "uneval")
-  aes$bb <- NULL
-  bb$x$mapping <- aes
+  mapping <- structure(as.list(match.call()[-1]), class = "uneval")
+  mapping$bb <- NULL
+  bb$x$mapping <- mapping
+  bb
+}
+
+#' @rdname billboard-aes
+#' @export
+bb_aes_string <- function(bb, x, y, group = NULL) {
+  mapping <- list()
+  if (!missing(x)) 
+    mapping["x"] <- list(x)
+  if (!missing(y)) 
+    mapping["y"] <- list(y)
+  if (!is.null(group)) 
+    mapping["group"] <- list(group)
+  mapping <- lapply(
+    X = mapping,
+    FUN = function(x) {
+      if (is.character(x)) {
+        parse(text = x)[[1]]
+      }
+      else {
+        x
+      }
+    }
+  )
+  bb$x$mapping <- structure(mapping, class = "uneval")
   bb
 }
 
 #' @rdname billboard-aes
 #' @export
 bbaes <- function(x, y, group = NULL) {
-  aes <- structure(as.list(match.call()[-1]), class = "uneval")
+  mapping <- structure(as.list(match.call()[-1]), class = "uneval")
+  mapping
+}
+
+#' @rdname billboard-aes
+#' @export
+bbaes_string <- function(x, y, group = NULL) {
+  mapping <- list()
+  if (!missing(x)) 
+    mapping["x"] <- list(x)
+  if (!missing(y)) 
+    mapping["y"] <- list(y)
+  if (!is.null(group)) 
+    mapping["group"] <- list(group)
+  mapping <- lapply(
+    X = mapping,
+    FUN = function(x) {
+      if (is.character(x)) {
+        parse(text = x)[[1]]
+      }
+      else {
+        x
+      }
+    }
+  )
+  aes <- structure(mapping, class = "uneval")
   aes
 }
 
 
 bbmapping <- function(data, mapping) {
   
-  if (is.null(data))
-    return(list())
+  # if (is.null(data))
+  #   return(list())
   
   if (is.null(mapping$group)) {
     json <- lapply(
@@ -68,11 +118,11 @@ bbmapping <- function(data, mapping) {
     )
     names(json) <- as.character(unlist(mapping))
     x <- as.character(mapping$x)
-    if (anyDuplicated(json[[x]])) {
+    if (inherits(json[[x]], what = c("character", "factor")) & anyDuplicated(json[[x]])) {
       y <- as.character(mapping$y)
       json[[y]] <- tapply(X = json[[y]], INDEX = json[[x]], FUN = sum, na.rm = TRUE)
       json[[x]] <- names(json[[y]])
-      json[[y]] <- unname(json[[y]])
+      json[[y]] <- as.vector(unname(json[[y]]))
       message("Non unique values in '", x, "' : calculating sum of '", y, "'")
     }
   } else {
@@ -85,14 +135,21 @@ bbmapping <- function(data, mapping) {
     json <- lapply(
       X = stats::setNames(n_, n_),
       FUN = function(iii) {
-        y_ <- eval(mapping$y, envir = data_split[[iii]], enclos = parent.frame())
-        x_ <- eval(mapping$x, envir = data_split[[iii]], enclos = parent.frame())
-        idx <- match(x = x_un, table = x_, nomatch = nrow(data_split[[iii]])+1)
-        y_[idx]
+        if (!is.null(mapping$y)) {
+          y_ <- eval(mapping$y, envir = data_split[[iii]], enclos = parent.frame())
+          x_ <- eval(mapping$x, envir = data_split[[iii]], enclos = parent.frame())
+          idx <- match(x = x_un, table = x_, nomatch = nrow(data_split[[iii]])+1)
+          y_[idx]
+        } else {
+          eval(mapping$x, envir = data_split[[iii]], enclos = parent.frame())
+        }
       }
     )
-    x <- as.character(mapping$x)
-    json[[x]] <- x_un
+    if (!is.null(mapping$y)) {
+      x <- as.character(mapping$x)
+      json[[x]] <- x_un
+      json <- json[c(x, setdiff(names(json), x))]
+    }
   }
   
   return(json)
